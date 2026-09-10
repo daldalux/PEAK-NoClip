89b1265867a7f6308a20d13580e52f9891d9ffc2
daldalux <116430569+daldalux@users.noreply.github.com>
2026-09-10 19:27:53 +0500

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -5,75 +5,85 @@ using UnityEngine;
 
 namespace PeakCheat
 {
-    [BepInPlugin("com.myname.peaknoclip", "Peak Ultimate Network Noclip", "1.2.0")]
+    [BepInPlugin("com.myname.peaknoclip", "Peak Ultimate Network Noclip", "1.6.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static bool IsFlyMode = false;
         public static Vector3 FlyDirection = Vector3.zero;
         public static CharacterMovement LocalPlayer = null;
-
-
         public static ConfigEntry<float> FlySpeed;
 
         private void Awake()
         {
-
             FlySpeed = Config.Bind("General", "FlySpeed", 50f, "Скорость полета персонажа");
-
             var harmony = new Harmony("com.myname.peaknoclip");
             harmony.PatchAll();
-            Logger.LogInfo($"Чит на сетевой полет загружен! F3 - Вкл/Выкл. +/- - Изменение скорости. Текущая: {FlySpeed.Value}");
         }
 
         private void Update()
         {
+            if (!Application.isFocused)
+            {
+                if (IsFlyMode)
+                {
+                    IsFlyMode = false;
+                    FlyDirection = Vector3.zero;
+                    ResetPhysics(false);
+                }
+                return;
+            }
 
             if (Input.GetKeyDown(KeyCode.F3))
             {
                 IsFlyMode = !IsFlyMode;
-                Debug.Log("[PEAK ЧИТ] Сетевой Ноуклип: " + IsFlyMode);
+                ResetPhysics(IsFlyMode);
+            }
 
-                if (LocalPlayer != null)
+            if (IsFlyMode)
+            {
+                if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadPlus))
                 {
-                    var allRbs = LocalPlayer.GetComponentsInChildren<Rigidbody>();
-                    var myColliders = LocalPlayer.GetComponentsInChildren<Collider>();
+                    FlySpeed.Value += 5f;
+                }
 
-                    foreach (var rb in allRbs)
-                    {
-                        if (rb != null)
-                        {
-                            rb.useGravity = !IsFlyMode;
-                            rb.linearVelocity = Vector3.zero;
-                            rb.angularVelocity = Vector3.zero;
-                        }
-                    }
+                if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus))
+                {
+                    FlySpeed.Value = Mathf.Max(5f, FlySpeed.Value - 5f);
+                }
+            }
+        }
 
-                    foreach (var col in myColliders)
+        private void ResetPhysics(bool flyState)
+        {
+            if (LocalPlayer == null || LocalPlayer.gameObject == null)
+            {
+                LocalPlayer = null;
+                return;
+            }
+
+            var allRbs = LocalPlayer.GetComponentsInChildren<Rigidbody>();
+            if (allRbs != null)
+            {
+                foreach (var rb in allRbs)
+                {
+                    if (rb != null)
                     {
-                        if (col != null)
-                        {
-                            col.isTrigger = IsFlyMode;
-                        }
+                        rb.useGravity = !flyState;
+                        rb.linearVelocity = Vector3.zero;
+                        rb.angularVelocity = Vector3.zero;
                     }
                 }
             }
 
-
-            if (IsFlyMode)
+            var myColliders = LocalPlayer.GetComponentsInChildren<Collider>();
+            if (myColliders != null)
             {
-
-                if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadPlus))
+                foreach (var col in myColliders)
                 {
-                    FlySpeed.Value += 5f;
-                    Debug.Log($"[PEAK ЧИТ] Скорость полета увеличена: {FlySpeed.Value}");
-                }
-
-
-                if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus))
-                {
-
-                    FlySpeed.Value = Mathf.Max(5f, FlySpeed.Value - 5f);
-                    Debug.Log($"[PEAK ЧИТ] Скорость полета уменьшена: {FlySpeed.Value}");
+                    if (col != null)
+                    {
+                        col.isTrigger = flyState;
+                    }
                 }
             }
         }
@@ -85,73 +95,60 @@ namespace PeakCheat
         [HarmonyPrefix]
         static bool Prefix(CharacterMovement __instance)
         {
-            if (__instance == null) return true;
-
-
-            if (!Application.isFocused)
-            {
-                Plugin.FlyDirection = Vector3.zero;
-                return true;
-            }
+            if (__instance == null || !Application.isFocused) return true;
 
             var characterField = AccessTools.Field(typeof(CharacterMovement), "character");
             if (characterField == null) return true;
 
             Character player = characterField.GetValue(__instance) as Character;
+            if (player == null || Character.localCharacter == null || player != Character.localCharacter) return true;
 
-            if (player != null && Character.localCharacter != null && player == Character.localCharacter)
+            Plugin.LocalPlayer = __instance;
+
+            if (Plugin.IsFlyMode)
             {
-                Plugin.LocalPlayer = __instance;
-
-                if (Plugin.IsFlyMode)
+                var dataField = AccessTools.Field(typeof(Character), "data");
+                if (dataField != null)
                 {
-
-                    var dataField = AccessTools.Field(typeof(Character), "data");
-                    if (dataField != null)
+                    var dataObj = dataField.GetValue(player);
+                    if (dataObj != null)
                     {
-                        var dataObj = dataField.GetValue(player);
-                        if (dataObj != null)
+                        var sinceGroundedField = AccessTools.Field(dataObj.GetType(), "sinceGrounded");
+                        if (sinceGroundedField != null)
                         {
-                            var sinceGroundedField = AccessTools.Field(dataObj.GetType(), "sinceGrounded");
-                            if (sinceGroundedField != null)
-                            {
-                                sinceGroundedField.SetValue(dataObj, 0f);
-                            }
+                            sinceGroundedField.SetValue(dataObj, 0f);
                         }
                     }
+                }
 
-                    Camera mainCam = Camera.main;
-                    if (mainCam == null || mainCam.transform == null)
-                    {
-                        Plugin.FlyDirection = Vector3.zero;
-                        return true;
-                    }
+                Camera mainCam = Camera.main;
+                if (mainCam == null || mainCam.transform == null)
+                {
+                    Plugin.FlyDirection = Vector3.zero;
+                    return true;
+                }
 
-                    Vector3 moveDir = Vector3.zero;
-                    bool hasInput = false;
+                Vector3 moveDir = Vector3.zero;
+                bool hasInput = false;
 
+                Vector3 camForward = Vector3.Scale(mainCam.transform.forward, new Vector3(1, 0, 1)).normalized;
+                Vector3 camRight = Vector3.Scale(mainCam.transform.right, new Vector3(1, 0, 1)).normalized;
 
-                    Vector3 camForward = Vector3.Scale(mainCam.transform.forward, new Vector3(1, 0, 1)).normalized;
-                    Vector3 camRight = Vector3.Scale(mainCam.transform.right, new Vector3(1, 0, 1)).normalized;
+                if (Input.GetKey(KeyCode.W)) { moveDir += camForward; hasInput = true; }
+                if (Input.GetKey(KeyCode.S)) { moveDir -= camForward; hasInput = true; }
+                if (Input.GetKey(KeyCode.D)) { moveDir += camRight; hasInput = true; }
+                if (Input.GetKey(KeyCode.A)) { moveDir -= camRight; hasInput = true; }
 
+                if (Input.GetKey(KeyCode.Space)) { moveDir += Vector3.up; hasInput = true; }
+                if (Input.GetKey(KeyCode.LeftControl)) { moveDir -= Vector3.up; hasInput = true; }
 
-                    if (Input.GetKey(KeyCode.W)) { moveDir += camForward; hasInput = true; }
-                    if (Input.GetKey(KeyCode.S)) { moveDir -= camForward; hasInput = true; }
-                    if (Input.GetKey(KeyCode.D)) { moveDir += camRight; hasInput = true; }
-                    if (Input.GetKey(KeyCode.A)) { moveDir -= camRight; hasInput = true; }
-
-
-                    if (Input.GetKey(KeyCode.Space)) { moveDir += Vector3.up; hasInput = true; }
-                    if (Input.GetKey(KeyCode.LeftControl)) { moveDir -= Vector3.up; hasInput = true; }
-
-                    if (hasInput)
-                    {
-                        Plugin.FlyDirection = moveDir.normalized;
-                    }
-                    else
-                    {
-                        Plugin.FlyDirection = Vector3.zero;
-                    }
+                if (hasInput)
+                {
+                    Plugin.FlyDirection = moveDir.normalized;
+                }
+                else
+                {
+                    Plugin.FlyDirection = Vector3.zero;
                 }
             }
             return true;
@@ -164,10 +161,8 @@ namespace PeakCheat
         [HarmonyPrefix]
         static bool Prefix(CharacterMovement __instance)
         {
-
             if (Plugin.IsFlyMode && Application.isFocused && __instance != null && __instance == Plugin.LocalPlayer)
             {
-
                 var rb = __instance.GetComponent<Rigidbody>();
                 if (rb == null) rb = __instance.GetComponentInChildren<Rigidbody>();
 
@@ -176,11 +171,9 @@ namespace PeakCheat
                     rb.linearVelocity = Plugin.FlyDirection * Plugin.FlySpeed.Value;
                     rb.angularVelocity = Vector3.zero;
                 }
-
                 return true;
             }
             return true;
         }
     }
-
 }
